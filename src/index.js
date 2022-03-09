018d934281a2ab6f819386cfd5ca889412d5a0f5
mandi-riegle <mandi.riegle@gmail.com>
2022-03-09 14:45:28 -0800

    Updated API keys

--- src/index.js
+++ src/index.js
@@ -68,7 +68,7 @@ function formatDate(timestamp) {
   }
   
   function getForecast(coordinates) {
-    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiKey = "aac0ab59bafcae8914960aa0936f89e3";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayForecast);
   }
@@ -100,7 +100,7 @@ function formatDate(timestamp) {
   }
   
   function search(city) {
-    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiKey = "aac0ab59bafcae8914960aa0936f89e3";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayTemperature);
   }
